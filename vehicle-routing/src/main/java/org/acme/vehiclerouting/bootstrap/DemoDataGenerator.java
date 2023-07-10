228c95676e579df3c813238e9d01ff197af39826
Rüdiger Sczuka <ruediger.sczuka@gmail.com>
2023-07-10 20:53:26 +0200

    Print distance between customers

--- vehicle-routing/src/main/java/org/acme/vehiclerouting/bootstrap/DemoDataGenerator.java
+++ vehicle-routing/src/main/java/org/acme/vehiclerouting/bootstrap/DemoDataGenerator.java
@@ -1,5 +1,9 @@
 package org.acme.vehiclerouting.bootstrap;
 
+import java.util.List;
+
+import org.acme.vehiclerouting.domain.Customer;
+import org.acme.vehiclerouting.domain.Depot;
 import org.acme.vehiclerouting.domain.Location;
 import org.acme.vehiclerouting.domain.VehicleRoutingSolution;
 import org.acme.vehiclerouting.persistence.VehicleRoutingSolutionsRepository;
@@ -33,6 +37,17 @@ public class DemoDataGenerator {
                 .setSouthWestCorner(new Location(0L, 49.43069, 11.03332))
                 .setNorthEastCorner(new Location(0L, 49.49069, 11.13332))
                 .build();
+        
+        // Depot d = problem.getDepotList().get(0);
+        // List<Customer> customers = problem.getCustomerList();
+        // customers.forEach(c-> System.err.println("D -> "+ c.getId()+": " + d.getLocation().getDistanceTo(c.getLocation())));
+        // for(int i = 0; i < customers.size() -1 ; i++){
+        //     for(int j = i+1; j < customers.size(); j++){
+        //         System.err.println( customers.get(i).getId() +" -> "+ customers.get(j).getId()+": " + customers.get(i).getLocation().getDistanceTo(customers.get(j).getLocation()));
+        //     }
+        // }
+        // customers.forEach(c-> System.err.println("ID: " + c.getId() + ", Demand: "+ c.getDemand()+", Sevice Time: "+c.getServiceTime()+ ", Service Window: " + c.getBeginServiceWindow()+"/"+c.getEndServiceWindow()));
+
         repository.addAll(SequentialInsertion.solve(problem, 1, false));
         repository.addAll(SequentialInsertion.solve(problem, 99, true));
     }
